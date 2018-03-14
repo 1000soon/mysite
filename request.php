178e39bea22d4e536e0731d9994863156d9b5947
1000soon <1000soon@action2quare.com>
2018-03-14 10:29:55 +0900

    mailer
    mail

--- request.php
+++ request.php
@@ -1,5 +1,6 @@
 <?php
 require_once("conf/dbconfig.php");
+require_once("lib/PHPMailer/PHPMailerAutoload.php");
 include("conf/functions.php");
 
 $name = $_POST['vname'];
@@ -13,6 +14,36 @@ $ip = $_SERVER['REMOTE_ADDR'];
 if($p1=="" || $p2=="" || $p3==""){
 	alertBack("전화번호를 정확히 입력 해 주셔야 자세한 상담이 가능합니다."); exit;
 }
+
+// mail
+$message = "이름 : ".$name."<br>";
+$message .= "연락처 : ".$p1."-".$p2."-".$p3."<br>";
+$message .= "내용 : ".$desc;
+	
+$mail = new PHPMailer;
+$mail->SMTPSecure = 'ssl';
+$mail->isSMTP();
+$mail->SMTPDebug = 0;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+
+$mail->Username = "hcsoon0212@gmail.com";
+$mail->Password = "cjstns59";
+
+$mail->setFrom('hcsoon0212@gmail.com', 'BankList');
+$mail->addReplyTo('donotreply@gmail.com', 'BankList');
+$mail->addAddress('hcsoon0212@gmail.com', 'sojin04860@gmail.com');
+$mail->CharSet = "utf-8";
+$mail->Encoding = "base64";
+
+//Set the subject line
+$mail->Subject = '새로운 상담 신청이 접수되었습니다.';
+$mail->msgHTML($message, dirname(__FILE__));
+$mail->AltBody = $message;
+$mail->send();
+	
 $conn = new Connection();
 $dbh = $conn->setConnection();
 $query = "INSERT INTO tb_request (v_name, v_type, v_phone1, v_phone2, v_phone3, v_price, v_desc, ip_addr) VALUES (:name, :type, :p1, :p2, :p3, :price, :desc, :ip)";
@@ -26,11 +57,12 @@ $stmt->bindParam(":price", $price, PDO::PARAM_STR);
 $stmt->bindParam(":desc", $desc, PDO::PARAM_STR);
 $stmt->bindParam(":ip", $ip, PDO::PARAM_STR);
 try{
-$stmt->execute();
-$dbh = $conn->closeConn();
-apcu_clear_cache();	
-location_replace("상담 신청이 접수 되었습니다.", "index.html"); exit;
+	$stmt->execute();
+	$dbh = $conn->closeConn();
+	location_replace("상담 신청이 접수 되었습니다.", "index.php"); exit;
+	apcu_clear_cache();	
 }catch(PDOException $e){
-alertBack("죄송합니다. 현재 일시적으로 장애가 발생하고 있습니다. 잠시 후 다시 시도 해 주시기 바랍니다."); exit;
+	alertBack("죄송합니다. 현재 일시적으로 장애가 발생하고 있습니다. 잠시 후 다시 시도 해 주시기 바랍니다."); exit;
 }
+
 ?>
