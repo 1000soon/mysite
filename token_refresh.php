27d4f62191c7d9178f0ab1baf37ea8b3e7d39281
1000soon <1000soon@action2quare.com>
2018-09-03 11:14:15 +0900

    토큰 갱신
    crontab 등록

--- token_refresh.php
+++ token_refresh.php
@@ -1,10 +1,10 @@
 <?php
 //$token = "22UTlls847FHJycZcFotDMZ89gS2xn1Uecj3mwoqAuYAAAFlid3FQg";
 $token = file_get_contents("upload/refresh.txt");
-$url = trim("https://kauth.kakao.com/oauth/token");
-$cmd ="curl -d 'grant_type=refresh_token' \
+$url = "https://kauth.kakao.com/oauth/token";
+$cmd = preg_replace("/\r/","","curl -d 'grant_type=refresh_token' \
  -d 'client_id=52ea8ba1a85b1fea2a9a7a9da7f7afe2' \
- -d 'refresh_token=".$token."' \ -H 'Content-type: application/x-www-form-urlencoded;charset=UTF-8' \ -H 'Accept: application/json' \ -v -X POST ".$url;
+ -d 'refresh_token=".$token."' \ -H 'Content-type: application/x-www-form-urlencoded;charset=UTF-8' \ -H 'Accept: application/json' \ -v -X POST ".$url);
 $resultSet = json_decode(shell_exec($cmd));
 print_r($resultSet);
 
